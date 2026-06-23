11c68ded13bbf21109c2323ebacaa609d1769491
Himansu0101 <himansusingh7600@gmail.com>
2026-06-23 23:38:36 +0530

    Create assignment02

---
+++ day01/assignment02.js
@@ -0,0 +1,19 @@
+console.log("Start small. Ship something.");
+
+let genderType = "Female"
+
+function printGender() {
+    
+    let color = "brown";
+    
+    if (genderType === "Female"){
+        var age = 30;
+        let color = "pink";
+        console.log( "color is",color);
+    }
+     console.log("age is", age);
+}
+printGender()
+
+ genderType = "Male"
+console.log("Gender is",genderType);
